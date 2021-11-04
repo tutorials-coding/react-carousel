99621de91e35f33d78525af26929872984bbf0c5
Vadim <korobeinikovv@mail.ru>
2021-11-04 19:04:55 +0300

    Add arrow buttons

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -1,10 +1,18 @@
 import { useEffect, useState, Children, cloneElement } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import './Carousel.css'
 
 const PAGE_WIDTH = 450
 
 export const Carousel = ({ children }) => {
   const [items, setItems] = useState([])
+
+  const handleLeftArrowClick = () => {
+    console.log('handleLeftArrowClick')
+  }
+  const handleRightArrowClick = () => {
+    console.log('handleLeftArrowClick')
+  }
 
   useEffect(() => {
     setItems(
@@ -22,9 +30,11 @@ export const Carousel = ({ children }) => {
 
   return (
     <div className="main-container">
+      <FaChevronLeft className="arrow" onClick={handleLeftArrowClick} />
       <div className="window">
         <div className="all-items-container">{items}</div>
       </div>
+      <FaChevronRight className="arrow" onClick={handleRightArrowClick} />
     </div>
   )
 }
