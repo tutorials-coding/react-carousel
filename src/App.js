0c726ff29b1ef1e9599f457c1a94947b0369f887
Vadim <korobeinikovv@mail.ru>
2021-11-06 19:41:34 +0300

    Add usage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import Carousel from './components/carousel-base'
+import CarouselCompound from './components/carousel-compound'
 
 export default function App() {
   return (
@@ -9,6 +10,17 @@ export default function App() {
         <div className="item item-2">Item 2</div>
         <div className="item item-3">Item 3</div>
       </Carousel>
+      <CarouselCompound>
+        <CarouselCompound.Page>
+          <div className="item item-1">Item 1</div>
+        </CarouselCompound.Page>
+        <CarouselCompound.Page>
+          <div className="item item-2">Item 2</div>
+        </CarouselCompound.Page>
+        <CarouselCompound.Page>
+          <div className="item item-3">Item 3</div>
+        </CarouselCompound.Page>
+      </CarouselCompound>
     </div>
   )
 }
