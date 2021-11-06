651fb1afc12d31c6b0324f2bb510255ef80af5d6
Vadim <korobeinikovv@mail.ru>
2021-11-06 19:39:20 +0300

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Carousel from './components/carousel-base'
 
 export default function App() {
   return (
-    <Carousel>
-      <div className="item item-1">Item 1</div>
-      <div className="item item-2">Item 2</div>
-      <div className="item item-3">Item 3</div>
-    </Carousel>
+    <div className="app__main-container">
+      <Carousel>
+        <div className="item item-1">Item 1</div>
+        <div className="item item-2">Item 2</div>
+        <div className="item item-3">Item 3</div>
+      </Carousel>
+    </div>
   )
 }
