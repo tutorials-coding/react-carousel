6d106c9a38ff6f306a315e689882487b97698a12
Vadim <korobeinikovv@mail.ru>
2021-11-07 17:39:13 +0300

    Code cleanup (remove base carousel)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css'
-import Carousel from './components/carousel-base'
 import CarouselCompound from './components/carousel-compound'
 
 export default function App() {
   return (
     <div className="app__main-container">
-      <Carousel>
-        <div className="item item-1">Item 1</div>
-        <div className="item item-2">Item 2</div>
-        <div className="item item-3">Item 3</div>
-      </Carousel>
       <CarouselCompound>
         <CarouselCompound.Page>
           <div className="item item-1">Item 1</div>
